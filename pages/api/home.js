161c998664077cb6ee6c74e1a029a0a38bfd62f1
dev-prospekt <dev@prospekt.hr>
2023-01-12 14:47:14 +0100

    add api

---
+++ pages/api/home.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+    const response = await fetch('http://strapi.wine-co.hr/api/homepage?populate=*')
+
+    const data = await response.json()
+    res.status(200).json(data)
+}
+  
