fedbc207d9257456a4dfeb47c8d621dd487ef6f0
dev-prospekt <dev@prospekt.hr>
2023-01-17 12:27:34 +0100

    aboutus top

--- components/aboutUs.js
+++ components/aboutUs.js
@@ -21,7 +21,7 @@ export default function AboutUs({data}) {
     return (
         <div className="aboutus grid grid-cols-2 h-full">
 
-            <div className="flex flex-col justify-center items-start relative mr-36 mt-40 max-[600px]:mt-36">
+            <div className="flex flex-col justify-center items-start relative mr-36 mt-40 max-[600px]:mt-32">
                 <h1 className='font-butlerregular text-5xl text-original max-[600px]:text-3xl uppercase'>
                 { aboutUsData.attributes?.title }
                 </h1>
